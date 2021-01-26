7ffe80d79fc0b3ac30d1bad4ee5ab4da9c8ba501
Barnaby Collins <barnabyc@sky.com>
2021-01-26 19:13:52 +0000

    Camera move with player

---
+++ MGD/Assets/Scripts/cameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    public float staticCameraRegion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float distanceFromPlayer = player.transform.position.x - transform.position.x;
+
+        float distanceOutsideRegion = Mathf.Abs(distanceFromPlayer) - staticCameraRegion;
+
+        if (distanceOutsideRegion > 0) {
+            float distanceToMove = distanceOutsideRegion * (distanceFromPlayer / Mathf.Abs(distanceFromPlayer));
+            transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+        }
+    }
+}
