836e33a6e2882db29b4dfc9475dcc33bce0e4e36
Barnaby Collins <barnabyc@sky.com>
2021-01-30 12:49:20 +0000

    Add glasses

---
+++ MGD/Assets/Scripts/GlassesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassesController : MonoBehaviour
+{
+    public float depthOffset;
+    public float distance;
+
+    public GameObject depthCoordinator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjectDepth depthScript = depthCoordinator.GetComponent<ObjectDepth>();
+        float posY = depthScript.updateY(gameObject, 0.5f) + depthOffset;
+        
+        transform.position = new Vector2(distance, posY);
+    }
+}
