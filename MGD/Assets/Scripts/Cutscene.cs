56da621265150a36781c33e867c385aa65cec609
Barnaby Collins <barnabyc@sky.com>
2021-02-01 16:35:27 +0000

    Add cutscene

---
+++ MGD/Assets/Scripts/Cutscene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class Cutscene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        VideoPlayer video = GetComponent<VideoPlayer>();
+        video.loopPointReached += SwitchToNextScene;
+    }
+
+    void SwitchToNextScene(VideoPlayer vp) {
+        vp.targetCameraAlpha = 0;
+        StartCoroutine(sceneLoadingCoroutine());
+    }
+
+    IEnumerator sceneLoadingCoroutine() {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadSceneAsync("MainScene");
+    }
+}
